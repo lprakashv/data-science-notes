b55925ec43643c8c380a9b648b1134d307a533c9
lprakashv <lprakashv@users.noreply.github.com>
2026-07-21 07:20:57 +0000

    deploy: 499718c98b03361171cd4556e60faeade1e2d18b

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0eced60d.js',
+                path_to_root + 'searchindex-93e797ca.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
